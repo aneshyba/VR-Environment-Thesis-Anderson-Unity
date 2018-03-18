85effde784b6b0986f04f4c0b06b548ba149b383
aneshyba <annaneshyba@gmail.com>
2018-03-18 13:46:32 -0700

    Inital commit
    This commit contains everything I have done so far.

---
+++ Assets/Scripts/RightRotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightRotator : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(1, 0, 0) * Time.deltaTime * 3);
+	}
+}
