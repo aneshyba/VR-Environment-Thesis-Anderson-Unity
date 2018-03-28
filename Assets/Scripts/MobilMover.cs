a1feb666c0900040e7d28de70a058eb3901b9d22
aneshyba <annaneshyba@gmail.com>
2018-03-27 17:47:23 -0700

    Lots of new things

---
+++ Assets/Scripts/MobilMover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobilMover : MonoBehaviour {
+
+    private bool direction;
+    public int rotationSpeed;
+    public bool initialCond; 
+
+    // Use this for initialization
+    void Start () {
+        direction = initialCond; 
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        direction = !direction;
+    }
+    
+	// Update is called once per frame
+	void Update () {
+		if (direction)
+        {
+            transform.Rotate(new Vector3(0, 0, 1) * Time.deltaTime * rotationSpeed);
+        } else
+        {
+            transform.Rotate(new Vector3(0, 0, 1) * Time.deltaTime * -rotationSpeed);
+        }
+	}
+}
---
+++ Assets/Scripts/Sunlight.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sunlight : MonoBehaviour {
+
+    // setting a light object 
+    public Light lit;
+    
+    // getting inital conditions for the flicker
+    // min cannot be negative
+    // speed cannot be negative 
+    public bool initSign;
+    public float maxInten;
+    public float minInten;
+    public double speed; 
+
+    // intensity and increase amount
+    private double switchSign;
+    private float inten; 
+
+
+	// Use this for initialization
+	void Start () {
+        inten = (maxInten + minInten) / 2;
+        lit.intensity = inten ;
+        if (initSign)
+        {
+            switchSign = .01 * speed; 
+        } else
+        {
+            switchSign = -.01 * speed;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        inten = inten + (float) switchSign; 
+        lit.intensity = inten; 
+        SwitchSign(); 
+    }
+
+    // function switches sign of additive 
+    private void SwitchSign()
+    {
+        if (inten < minInten || inten > maxInten)
+        {
+            switchSign = -switchSign; 
+        }
+    }
+}
